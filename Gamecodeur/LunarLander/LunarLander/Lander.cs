df5baf2296aa2dda080ef4338e96b5124f93fad8
arkeid0s <matproz.gaming@gmail.com>
2023-03-11 19:17:18 +0100

    Lunar lander ship base

---
+++ Gamecodeur/LunarLander/LunarLander/Lander.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunarLander
+{
+	internal class Lander
+	{
+		public Texture2D img { get; set; }
+		public Texture2D imgEngine { get; set; }
+
+		public Vector2 position { get; set; } = Vector2.Zero;
+		public Vector2 origin { get => new(img.Width / 2, img.Height / 2); }
+		public Vector2 originEngine { get => new((imgEngine.Width / 2) + img.Width / 2, imgEngine.Height / 2); }
+		public Vector2 velocity { get; set; } = Vector2.Zero;
+		public float speed { get; set; } = 0.02f;
+		public float angle { get; set; } = 0f;
+		public bool isEngineOn { get; set; } = false;
+		private float _maxSpeed = 2f;
+
+		public void Update()
+		{
+			velocity += new Vector2(0, 0.005f);
+
+			if (Math.Abs(velocity.X) >= _maxSpeed)
+            {
+                velocity = new Vector2(velocity.X < 0 ? 0 - _maxSpeed : _maxSpeed, velocity.Y);
+            }
+
+			if (Math.Abs(velocity.Y) >= _maxSpeed)
+			{
+				velocity = new Vector2(velocity.X, velocity.Y < 0 ? 0 - _maxSpeed : _maxSpeed);
+			}
+
+			position += velocity;
+		}
+	}
+}
---
+++ Gamecodeur/LunarLander/LunarLander/MainGame.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace LunarLander
+{
+	public class MainGame : Game
+	{
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+
+		private KeyboardState _oldKeyboardState;
+		private KeyboardState _newKeyboardState;
+
+		private Lander _player;
+
+		public MainGame()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			_player = new Lander
+			{
+				position = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2)
+			};
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			_player.img = Content.Load<Texture2D>("ship");
+			_player.imgEngine = Content.Load<Texture2D>("engine");
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			_newKeyboardState = Keyboard.GetState();
+
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+            if (_newKeyboardState.IsKeyDown(Keys.Right))
+            {
+                _player.angle += 2f;
+            }
+
+            if (_newKeyboardState.IsKeyDown(Keys.Left))
+            {
+                _player.angle -= 2f;
+            }
+
+			if (_newKeyboardState.IsKeyDown(Keys.Up))
+            {
+                _player.isEngineOn = true;
+
+				float angle = MathHelper.ToRadians(_player.angle);
+				float xForce = (float)Math.Cos(angle);
+				float yForce = (float)Math.Sin(angle);
+
+				_player.velocity += new Vector2(xForce, yForce) * _player.speed;
+            }
+            else
+            {
+                _player.isEngineOn = false;
+            }
+
+			_player.Update();
+
+            if (_player.position.X < 0)
+            {
+                _player.position = new Vector2(0, _player.position.Y);
+				_player.velocity = new Vector2(0 - _player.velocity.X, _player.velocity.Y);
+            }
+            if (_player.position.X >= GraphicsDevice.Viewport.Width)
+            {
+                _player.position = new Vector2(GraphicsDevice.Viewport.Width, _player.position.Y);
+				_player.velocity = new Vector2(0 - _player.velocity.X, _player.velocity.Y);
+            }
+            if (_player.position.Y < 0)
+            {
+                _player.position = new Vector2(_player.position.X, 0);
+				_player.velocity = new Vector2(_player.velocity.X, 0 - _player.velocity.Y);
+            }
+            if (_player.position.Y >= GraphicsDevice.Viewport.Height)
+            {
+				_player.position = new Vector2(_player.position.X, GraphicsDevice.Viewport.Height);
+				_player.velocity = new Vector2(_player.velocity.X, 0 - _player.velocity.Y);
+			}
+
+            _oldKeyboardState = _newKeyboardState;
+            base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.DarkGray);
+
+			_spriteBatch.Begin();
+
+			if (_player.isEngineOn)
+				_spriteBatch.Draw(_player.imgEngine, _player.position, null, Color.White, MathHelper.ToRadians(_player.angle), _player.originEngine, Vector2.One, SpriteEffects.None, 0f);
+			_spriteBatch.Draw(_player.img, _player.position, null, Color.White, MathHelper.ToRadians(_player.angle), _player.origin, Vector2.One, SpriteEffects.None, 0f);
+
+			_spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
